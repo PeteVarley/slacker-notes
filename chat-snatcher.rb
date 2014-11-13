7f3ce71141e3121f7b0113b961f46e4dc8ce3b8b
Varley <petevarley@gmail.com>
2014-11-13 10:13:22 -0500

    manually saving

--- chat-snatcher.rb
+++ chat-snatcher.rb
@@ -3,6 +3,7 @@ require 'sinatra/partial'
 require 'better_errors'
 require 'slack/client'
 
+
 require_relative 'config/dotenv'
 require_relative 'models'
 
@@ -10,8 +11,19 @@ Dotenv.load
 
 SLACK_API_TOKEN=ENV["SLACK"]
 
-client = Slack::Client.new(token: SLACK_API_TOKEN)
+# client = Slack::Client.new(token: SLACK_API_TOKEN)
 
-puts client.users.list
-#puts client.channels.list
-#puts client.channels.history('C1234567')
+# puts client.users.list
+# puts client.channels.list
+# puts client.channels.history('C1234567')
+def save_info
+  client = Slack::Client.new(token: SLACK_API_TOKEN)
+  #puts client.users.list
+  me = "Pete"
+  r = Record.get([3])
+  n = Note.new(:note_title=>"new title",:note_subjects=>"list",:note_content=>client.users.list)
+  r.notes << n
+  r.save
+end
+
+save_info
