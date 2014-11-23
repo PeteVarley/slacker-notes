6ffb638b36f1a47bc5fdb7802c235aae40ea053d
Varley <petevarley@gmail.com>
2014-11-22 19:36:23 -0500

    Working with POST /chats, user passes in desired amount of chats to save on the home page (/)

--- chat-snatcher.rb
+++ chat-snatcher.rb
@@ -20,28 +20,37 @@ helpers do
 end
 
 get "/" do
-  @archives = default_record.archives
 
   erb :home
 end
 
-get "/chats/:num" do
-  num = params[:num]
-  puts "num"
-  puts num
+get "/archives" do
+  @archives = default_record.archives
+
+  erb :archives
+end
+
+post "/chats" do
   @archives = default_record.archives
   @current_archive = Archive.create(:ts => Time.now)
   @archives << @current_archive
   @archives.save
-  puts 'archives'
-  puts @archives
-  puts 'current archive id'
+
   puts @current_archive.id
 
+  number_param = params().fetch("number")
+
+  puts 'number'
+  number = number_param[:chat_number]
+  p number
+
+
   client = Slack::Client.new(token: SLACK_API_TOKEN)
-  @message_data = JSON.parse(client.channels.history(:channel=>ENV["SLACK_CHANNEL"],:count=>num))
+  @message_data = JSON.parse(client.channels.history(:channel=>ENV["SLACK_CHANNEL"],:count=>number))
 
   @messages_data = @message_data["messages"]
+
+
 
   @messages_data.count.times do |x|
 
@@ -77,11 +86,63 @@ get "/chats/:num" do
   end
 
   if @chat.saved?()
-    erb(:chats)
+    redirect "/archive/#{@current_archive.id}"
   else
     redirect "/"
   end
+end
 
+post "/chats/:num" do
+  num = params[:num]
+  @archives = default_record.archives
+  @current_archive = Archive.create(:ts => Time.now)
+  @archives << @current_archive
+  @archives.save
+
+  client = Slack::Client.new(token: SLACK_API_TOKEN)
+  @message_data = JSON.parse(client.channels.history(:channel=>ENV["SLACK_CHANNEL"],:count=>num))
+
+  @messages_data = @message_data["messages"]
+
+  # @messages_data.count.times do |x|
+
+  #   message_hash = @messages_data[x]
+
+
+  #   @user = message_hash["user"]
+
+  #   @text = message_hash["text"]
+
+  #   @ts = message_hash["ts"]
+
+  #   @chat = Chat.create(:text => @text)
+
+  #   @current_archive.chats << @chat
+
+
+  #   if @current_archive.save
+  #    # my_account is valid and has been saved
+  #   else
+  #     puts 'chats errors any'
+  #     puts @current_archive.chats.any? { |chat| chat.errors.any? }
+
+  #     @current_archive.chats.each do |chat|
+  #      chat.errors.each do |error|
+  #        p error
+  #      end
+  #    end
+  #  end
+
+
+
+  # end
+
+  # if @chat.saved?()
+  #   erb(:chats)
+  # else
+  #   redirect "/"
+  # end
+  erb(:chats)
 end
 
 get("/archive/:id") do
